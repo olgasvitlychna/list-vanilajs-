9f3d5c9062806665fb3c3649854c7ba3dd8a5e15
olgasvitlychna <olga.svitlychna9@gmail.com>
2022-03-21 11:08:06 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,235 @@
+let employeesList = {
+    "employees": [
+        {
+            "section": "Pododdział 1", "id": "1", "group": [
+                {
+                    "groupNumber": "Zespół 1", "groupName": "HR", "subgroups": [
+                        { "subgroupsNumber": "Podzespół 1.1", "subgroupsName": "Administacja", },
+                        { "subgroupsNumber": "Podzespół 1.2", "subgroupsName": "Rekruter" },
+                        { "subgroupsNumber": "Podzespół 1.3", "subgroupsName": "Planowanie" },
+                        { "subgroupsNumber": "Podzespół 1.4", "subgroupsName": "Kadrowa" },
+                        { "subgroupsNumber": "Podzespół 1.5", "subgroupsName": "Psycholog" }
+                    ]
+                },
+                {
+                    "groupNumber": "Zespół 2", "groupName": "Marketing", "subgroups": [
+                        { "subgroupsNumber": "Podzespół 2.1", "subgroupsName": "Strategia" },
+                        { "subgroupsNumber": "Podzespół 2.2", "subgroupsName": "Usługi" },
+                        { "subgroupsNumber": "Podzespół 2.3", "subgroupsName": "Sprzedaż" },
+                        { "subgroupsNumber": "Podzespół 2.4", "subgroupsName": "Call-center" },
+                        { "subgroupsNumber": "Podzespół 2.5", "subgroupsName": "Szukaj" }
+                    ]
+                },
+                {
+                    "groupNumber": "Zespół 3", "groupName": "Analitycy", "subgroups": [
+                        { "subgroupsNumber": "Podzespół 3.1", "subgroupsName": "Kalendarz" },
+                        { "subgroupsNumber": "Podzespół 3.2", "subgroupsName": "Kokpit" },
+                        { "subgroupsNumber": "Podzespół 3.3", "subgroupsName": "Widok" },
+                        { "subgroupsNumber": "Podzespół 3.4", "subgroupsName": "Róności" },
+                        { "subgroupsNumber": "Podzespół 3.5", "subgroupsName": "Szukaj" }
+                    ]
+                },
+                {
+                    "groupNumber": "Zespół 4", "groupName": "Programiści", "subgroups": [
+                        { "subgroupsNumber": "Podzespół 4.1", "subgroupsName": "Front-end" },
+                        { "subgroupsNumber": "Podzespół 4.2", "subgroupsName": "Middle-front-end" },
+                        { "subgroupsNumber": "Podzespół 4.3", "subgroupsName": "Middle-end" },
+                        { "subgroupsNumber": "Podzespół 4.4", "subgroupsName": "Middle-back-end" },
+                        { "subgroupsNumber": "Podzespół 4.5", "subgroupsName": "Back-end" }
+                    ]
+                }
+            ]
+        },
+        {
+            "section": "Pododdział 2", "id": 2, "group": [
+                {
+                    "groupNumber": "Zespół 1", "groupName": "HR", "subgroups": [
+                        { "subgroupsNumber": "Podzespół 1.1", "subgroupsName": "Administacja" },
+                        { "subgroupsNumber": "Podzespół 1.2", "subgroupsName": "Rekruter" },
+                        { "subgroupsNumber": "Podzespół 1.3", "subgroupsName": "Planowanie" },
+                        { "subgroupsNumber": "Podzespół 1.4", "subgroupsName": "Kadrowa" },
+                        { "subgroupsNumber": "Podzespół 1.5", "subgroupsName": "Psycholog" }
+                    ]
+                },
+                {
+                    "groupNumber": "Zespół 2", "groupName": "Marketing", "subgroups": [
+                        { "subgroupsNumber": "Podzespół 2.1", "subgroupsName": "Strategia" },
+                        { "subgroupsNumber": "Podzespół 2.2", "subgroupsName": "Usługi" },
+                        { "subgroupsNumber": "Podzespół 2.3", "subgroupsName": "Sprzedaż" },
+                        { "subgroupsNumber": "Podzespół 2.4", "subgroupsName": "Call-center" },
+                        { "subgroupsNumber": "Podzespół 2.5", "subgroupsName": "Szukaj" }
+                    ]
+                },
+                {
+                    "groupNumber": "Zespół 3", "groupName": "Analitycy", "subgroups": [
+                        { "subgroupsNumber": "Podzespół 3.1", "subgroupsName": "Kalendarz" },
+                        { "subgroupsNumber": "Podzespół 3.2", "subgroupsName": "Kokpit" },
+                        { "subgroupsNumber": "Podzespół 3.3", "subgroupsName": "Widok" },
+                        { "subgroupsNumber": "Podzespół 3.4", "subgroupsName": "Róności" },
+                        { "subgroupsNumber": "Podzespół 3.5", "subgroupsName": "Szukaj" }
+                    ]
+                },
+                {
+                    "groupNumber": "Zespół 4", "groupName": "Programiści", "subgroups": [
+                        { "subgroupsNumber": "Podzespół 4.1", "subgroupsName": "Front-end" },
+                        { "subgroupsNumber": "Podzespół 4.2", "subgroupsName": "Middle-front-end" },
+                        { "subgroupsNumber": "Podzespół 4.3", "subgroupsName": "Middle-end" },
+                        { "subgroupsNumber": "Podzespół 4.4", "subgroupsName": "Middle-back-end" },
+                        { "subgroupsNumber": "Podzespół 4.5", "subgroupsName": "Back-end" }
+                    ]
+                }
+            ]
+        },
+        {
+            "section": "Pododdział 3", "id": 3, "group": [
+                {
+                    "groupNumber": "Zespół 1", "groupName": "HR", "subgroups": [
+                        { "subgroupsNumber": "Podzespół 1.1", "subgroupsName": "Administacja" },
+                        { "subgroupsNumber": "Podzespół 1.2", "subgroupsName": "Rekruter" },
+                        { "subgroupsNumber": "Podzespół 1.3", "subgroupsName": "Planowanie" },
+                        { "subgroupsNumber": "Podzespół 1.4", "subgroupsName": "Kadrowa" },
+                        { "subgroupsNumber": "Podzespół 1.5", "subgroupsName": "Psycholog" }
+                    ]
+                },
+                {
+                    "groupNumber": "Zespół 2", "groupName": "Marketing", "subgroups": [
+                        { "subgroupsNumber": "Podzespół 2.1", "subgroupsName": "Strategia" },
+                        { "subgroupsNumber": "Podzespół 2.2", "subgroupsName": "Usługi" },
+                        { "subgroupsNumber": "Podzespół 2.3", "subgroupsName": "Sprzedaż" },
+                        { "subgroupsNumber": "Podzespół 2.4", "subgroupsName": "Call-center" },
+                        { "subgroupsNumber": "Podzespół 2.5", "subgroupsName": "Szukaj" }
+                    ]
+                },
+                {
+                    "groupNumber": "Zespół 3", "groupName": "Analitycy", "subgroups": [
+                        { "subgroupsNumber": "Podzespół 3.1", "subgroupsName": "Kalendarz" },
+                        { "subgroupsNumber": "Podzespół 3.2", "subgroupsName": "Kokpit" },
+                        { "subgroupsNumber": "Podzespół 3.3", "subgroupsName": "Widok" },
+                        { "subgroupsNumber": "Podzespół 3.4", "subgroupsName": "Róności" },
+                        { "subgroupsNumber": "Podzespół 3.5", "subgroupsName": "Szukaj" }
+                    ]
+                },
+                {
+                    "groupNumber": "Zespół 4", "groupName": "Programiści", "subgroups": [
+                        { "subgroupsNumber": "Podzespół 4.1", "subgroupsName": "Front-end" },
+                        { "subgroupsNumber": "Podzespół 4.2", "subgroupsName": "Middle-front-end" },
+                        { "subgroupsNumber": "Podzespół 4.3", "subgroupsName": "Middle-end" },
+                        { "subgroupsNumber": "Podzespół 4.4", "subgroupsName": "Middle-back-end" },
+                        { "subgroupsNumber": "Podzespół 4.5", "subgroupsName": "Back-end" }
+                    ]
+                }
+            ]
+        }
+    ]
+}
+
+
+let sectionList = document.getElementById('sectionList');
+let listHeader = document.createElement('li');
+listHeader.setAttribute('class', 'listHeader');
+let checkboks = document.createElement('input');
+checkboks.setAttribute('id', 'mainLabel');
+let label = document.createElement('label');
+label.setAttribute('for', 'mainLabel');
+listHeader.innerText = 'Nagłówek drzewa wielowierszowy';
+sectionList.appendChild(listHeader);
+
+
+for (let employee of employeesList.employees) {
+    let liElementForSection = document.createElement('li');
+    liElementForSection.setAttribute('class', 'mainSection');
+
+    let headerForSection = document.createElement('h3');
+    headerForSection.setAttribute('class', 'mainSectionHeader');
+    headerForSection.innerText = employee.section;
+
+    sectionList.appendChild(liElementForSection);
+    liElementForSection.appendChild(headerForSection);
+
+    let ulElement = document.createElement('ul');
+    ulElement.setAttribute('class', 'groupList');
+    liElementForSection.appendChild(ulElement);
+
+
+    employee.group.forEach((item, index) => {
+        let liElementForGroup = document.createElement('li');
+        liElementForGroup.setAttribute('class', 'groupSection');
+
+        let headerForGroup = document.createElement('h4');
+        headerForGroup.setAttribute('class', 'groupHeader');
+        headerForGroup.innerText = item.groupNumber;
+
+        let subHeaderForGroup = document.createElement('span');
+        subHeaderForGroup.setAttribute('class', 'groupSpan');
+        subHeaderForGroup.innerHTML = item.groupName;
+
+        ulElement.appendChild(liElementForGroup);
+        liElementForGroup.appendChild(headerForGroup);
+        headerForGroup.appendChild(subHeaderForGroup);
+
+
+        let ulElementForSubroup = document.createElement('ul');
+        liElementForGroup.appendChild(ulElementForSubroup);
+
+        if (index % 2 == 0) {
+            headerForGroup.classList.toggle('black')
+        }
+
+        item.subgroups.forEach((item, index) => {
+            let liElementForSubgroups = document.createElement('li');
+            liElementForSubgroups.setAttribute('class', 'subGroupSection');
+
+            let headerForSubGroup = document.createElement('h4');
+            headerForSubGroup.setAttribute('class', 'subGroupHeader');
+            headerForSubGroup.innerText = item.subgroupsNumber;
+
+            let spanForSubGroup = document.createElement('span');
+            spanForSubGroup.setAttribute('class', 'subGroupSpan');
+            spanForSubGroup.innerHTML = item.subgroupsName;
+
+            if (index % 2 == 0) {
+                headerForSubGroup.classList.toggle('black');
+            }
+
+            ulElementForSubroup.appendChild(liElementForSubgroups);
+            liElementForSubgroups.appendChild(headerForSubGroup);
+            headerForSubGroup.appendChild(spanForSubGroup);
+
+        })
+    })
+
+}
+document.querySelectorAll(".mainSectionHeader").forEach(item => {
+    item.addEventListener('click', () => {
+
+        const parent = item.parentNode;
+        if (parent.classList.contains('active')) {
+            parent.classList.remove('active');
+
+        } else {
+            document
+                .querySelectorAll('.mainSection')
+                .forEach((child) => child.classList.remove('active'))
+            parent.classList.add('active');
+            
+        }
+    })
+})
+document.querySelectorAll(".groupHeader").forEach(item => {
+    item.addEventListener('click', function () {
+        let panel = this.nextElementSibling;
+        this.classList.toggle('active');
+        this.classList.remove('black');
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+
+    })
+})
+
+
+
+
+
